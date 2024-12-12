412b8b9b7224323b7a2c9cf686d75f753b81cc09
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-12 14:12:31 +0700

    Enhance Tailwind configuration: Add responsive container settings, custom animations, and keyframes for marquee effects while maintaining existing color variables.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				xl: '1024px',
+				lg: '900px',
+				md: '768px',
+				sm: '640px',
+			},
+		},
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+			animation: {
+				marquee: 'marquee 20s linear infinite',
+				marquee2: 'marquee2 20s linear infinite',
+			},
+			keyframes: {
+				marquee: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-100%)' },
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0%)' },
+				},
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
