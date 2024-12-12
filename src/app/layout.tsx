7c0b45b102f5ce8c4098c4c112e67f75833ffe6e
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-12 14:12:48 +0700

    Import Font Awesome CSS in layout.tsx for enhanced icon support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./globals.css";
 
 const geistSans = Geist({
